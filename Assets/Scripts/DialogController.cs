57d472effbc237e640804ca41b2b63b8153337a4
Julian Hernandez <gmfk07@gmail.com>
2019-06-13 23:11:31 -0400

    Dialog Controller beginnings

---
+++ Assets/Scripts/DialogController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogController : Singleton<DialogController>
+{
+    public GameObject DialogPanel;
+
+    private Queue<string> currentDialog;
+    private bool isDisplayingDialog = false;
+
+    //If no dialog is being displayed, start displaying the provided text
+    public void BeginDialog(List<string> dialog)
+    {
+        isDisplayingDialog = true;
+        currentDialog = new Queue<string>(dialog);
+        DialogPanel.SetActive(true);
+        //Show text
+    }
+}
