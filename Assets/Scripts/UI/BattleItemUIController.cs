8c4503f8cdda35e6f6bdaaa18cf8cffdc6b04b22
Julian Hernandez <gmfk07@gmail.com>
2019-07-07 22:50:27 -0400

    Battle Icons

---
+++ Assets/Scripts/UI/BattleItemUIController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BattleItemUIController : MonoBehaviour
+{
+    [SerializeField] private Image UpAttackImage;
+    [SerializeField] private Image LeftAttackImage;
+    [SerializeField] private Image DownAttackImage;
+    [SerializeField] private Image RightAttackImage;
+
+    [SerializeField] private Image UpDefenseImage;
+    [SerializeField] private Image LeftDefenseImage;
+    [SerializeField] private Image DownDefenseImage;
+    [SerializeField] private Image RightDefenseImage;
+
+    private void Start()
+    {
+        UpAttackImage.sprite = PlayerStats.upAttack.itemSprite;
+        LeftAttackImage.sprite = PlayerStats.leftAttack.itemSprite;
+        DownAttackImage.sprite = PlayerStats.downAttack.itemSprite;
+        RightAttackImage.sprite = PlayerStats.rightAttack.itemSprite;
+
+        UpDefenseImage.sprite = PlayerStats.upDefense.itemSprite;
+        LeftDefenseImage.sprite = PlayerStats.leftDefense.itemSprite;
+        DownDefenseImage.sprite = PlayerStats.downDefense.itemSprite;
+        RightDefenseImage.sprite = PlayerStats.rightDefense.itemSprite;
+    }
+}
