0bec441c41e81f664de7fce8058fa294a8a414d9
Julian Hernandez <gmfk07@gmail.com>
2019-08-02 00:07:26 -0400

    Environment stuff, better camera tracking

--- Assets/Scripts/Overworld/CameraFollow.cs
+++ Assets/Scripts/Overworld/CameraFollow.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     [SerializeField] private float lerpSpeedX;
+    [SerializeField] private float lerpSpeedY;
     [SerializeField] private float lerpSpeedZ;
+
+    [SerializeField] private float yDistAbovePlayer;
     [SerializeField] private float zDistFromPlayer;
     [SerializeField] private GameObject player;
 
@@ -13,8 +16,9 @@ public class CameraFollow : MonoBehaviour
     {
         Vector3 cameraPos = transform.position;
         Vector3 playerPos = player.transform.position;
-        float x = Mathf.Lerp(cameraPos.x, playerPos.x, lerpSpeedX*Time.deltaTime);
-        float z = Mathf.Lerp(cameraPos.z, playerPos.z - zDistFromPlayer, lerpSpeedZ*Time.deltaTime);
-        transform.position = new Vector3(x, cameraPos.y, z);
+        float x = Mathf.Lerp(cameraPos.x, playerPos.x, lerpSpeedX * Time.deltaTime);
+        float y = Mathf.Lerp(cameraPos.y, playerPos.y + yDistAbovePlayer, lerpSpeedY * Time.deltaTime);
+        float z = Mathf.Lerp(cameraPos.z, playerPos.z - zDistFromPlayer, lerpSpeedZ * Time.deltaTime);
+        transform.position = new Vector3(x, y, z);
     }
 }
