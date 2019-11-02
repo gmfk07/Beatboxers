6d509312ed8fd2effb622aa8a0767dfa0ba2d446
Julian Hernandez <gmfk07@gmail.com>
2019-11-02 01:32:17 -0400

    Testing and Pop Rock work

---
+++ Assets/Scripts/Overworld/Overworld Enemies/EnemyJump.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyJump : MonoBehaviour
+{
+    private float startingY;
+    [SerializeField] private float jumpVelocity;
+    [SerializeField] private float gravityAccel; //change in velocity per second
+
+    private void Start()
+    {
+        startingY = transform.position.y;
+    }
+}
